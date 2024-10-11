59e5f73ee6c908d311f3e610e25fcb833a3865eb
Ali <alinajafian580@Gmail.com>
2024-10-11 12:23:45 +0330

    Made the Basic requirements to run the application and accept and validate commands on a basic level

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\tWelcome to Todo Tracker CLI or TTC for short! \n\tuse the [help] command to view a list of all commands available");
+
+
+        while (true) {
+            System.out.println("Awaiting Command Input ...........");
+            String fullCommand = scanner.nextLine();
+            String[] commandParts = fullCommand.split(" ", 2);
+            String keyWord = commandParts[0];
+            String rest = "";
+            if (keyWord.equalsIgnoreCase("help")) {
+                displayHelp();
+            } else {
+                try {
+                    rest = commandParts[1];
+                    if (rest == "") {
+                        throw new Exception("norest");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Where rest?");
+                }
+            }
+
+        }
+
+
+    }
+    public static void displayHelp() {
+        System.out.println("This is a helper message");
     }
 }
