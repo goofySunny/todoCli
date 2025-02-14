47f14b1069662e47638bbb495e949cce2ca93738
Najaf <Alinajafian580@Gmail.com>
2025-02-14 16:11:11 +0330

    Changes and optimizations

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,35 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("\tWelcome to Todo Tracker CLI or TTC for short! \n\tuse the [help] command to view a list of all commands available");
 
+        TodoAdditionService todoAdditionService = new TodoAdditionService();
 
         while (true) {
+
             System.out.println("Awaiting Command Input ...........");
             String fullCommand = scanner.nextLine();
             String[] commandParts = fullCommand.split(" ", 2);
             String keyWord = commandParts[0];
             String rest = "";
+
+
             if (keyWord.equalsIgnoreCase("help")) {
                 displayHelp();
             } else {
                 try {
-                    rest = commandParts[1];
-                    if (rest == "") {
-                        throw new Exception("norest");
-                    }
-                } catch (Exception e) {
-                    System.out.println("Where rest?");
+                        rest = commandParts[1];
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.printf("%10s%n", "Where Rest?");
                 }
+            }
+
+            if (keyWord.equalsIgnoreCase("exit")) {
+                return;
+            } else if (keyWord.equalsIgnoreCase("add")) {
+                todoAdditionService.addTodo(commandParts[1]);
             }
 
         }
